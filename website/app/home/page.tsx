7b394d47a107c34ffcbad0ddb102a4677048766f
reza-farmani <farmanir862@gmail.com>
2025-07-20 12:28:33 +0330

    Add animation components

--- website/app/home/page.tsx
+++ website/app/home/page.tsx
@@ -1,8 +1,12 @@
+import BlurText from "../ui/animation/BlurText";
+
 export default function Page() {
   return (
     <main >
       <div className="mb-12 text-4xl"></div>
-      <div className="bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden mb-6 h-screen mr-28 ml-28 "></div>
+      <div className="bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden mb-6 h-screen mr-28 ml-28 flex items-center justify-center" dir="rtl">
+        <BlurText text="بهترین هارو با ما تجربه کنید" delay={150} animateBy="words" direction="top" className="text-2xl mb-8"/>
+      </div>
     </main>
   );
 }
---
+++ website/app/ui/ButtonBest.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ButtonBest() {
+  return (
+    <button className='sans-bold bg-[#006BAD] text-[#e8f6ff] py-2 px-8 rounded-xl hover:bg-green-600 hover:scale-105 duration-300 cursor-pointer button-best'>ثبت سفارش</button>
+  )
+}
+
+export default ButtonBest
